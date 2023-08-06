742285d3c2ee215e09fa5b89e02917466a8a5d66
Raymond Tju <75233529+raymondtju@users.noreply.github.com>
2023-08-06 22:36:23 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,22 @@
 from flask import Flask
-app = Flask(__name__)
+
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.exceptions import ConvergenceWarning
+import json
+import warnings
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import matplotlib.pyplot as plt
+from pymongo import MongoClient 
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
+
+
+number = 1
 
 @app.route("/api/python")
-number = 1
 def hello_world():
     return number
+
+app = Flask(__name__)
